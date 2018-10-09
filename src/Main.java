0e4b3bbccb3afa3dec93223e16cc22dbea3220e3
riccool5573 <26188@ma-web.nl>
2018-10-09 15:30:31 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+ // array stuffs
+        String[] games = {"fallout: new vegas", "Arma 3", "rainbow six siege", "rimworld",};
+        int index = 0;
+        index+=0;
+
+        System.out.println(games[index]);
+    }
+}
