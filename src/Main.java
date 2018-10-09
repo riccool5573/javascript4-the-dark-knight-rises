27da0a327cbd14d85fada06418d927e4fc9b92f1
riccool5573 <26188@ma-web.nl>
2018-10-09 15:41:07 +0200

    extra game toegevoegd

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
  // array stuffs
-        String[] games = {"fallout: new vegas", "Arma 3", "rainbow six siege", "rimworld",};
+        String[] games = {"fallout: new vegas", "Arma 3", "rainbow six siege", "rimworld", "fallout 4"};
         int index = 0;
-        index+=0;
+        index+=3;
 
         System.out.println(games[index]);
     }
